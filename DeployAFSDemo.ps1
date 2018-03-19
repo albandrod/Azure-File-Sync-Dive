7b0aa988e6197f7b010a05d843a8fc929d06df7e
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2018-03-19 00:47:10 -0400

    Add files via upload

---
+++ DeployAFSDemo.ps1
@@ -0,0 +1,16 @@
+﻿Register-AzureRmProviderFeature `
+  -FeatureName AllowGlobalVnetPeering `
+  -ProviderNamespace Microsoft.Network
+
+  Get-AzureRmProviderFeature `
+  -FeatureName AllowGlobalVnetPeering `
+  -ProviderNamespace Microsoft.Network
+
+Register-AzureRmResourceProvider `
+  -ProviderNamespace Microsoft.Network
+
+  New-AzureRmResourceGroup -Location "canadacentral" -Name "rgAFSDemo"
+
+  $date = Get-Date -Format "yyyyMMddhhmmss"
+
+New-AzureRmResourceGroupDeployment -Name $date -ResourceGroupName "rgAFSDemo" -Mode Incremental -TemplateUri "https://raw.githubusercontent.com/swiftsolves-msft/Azure-File-Sync-Dive/master/AzureFileSyncDemo.json"
